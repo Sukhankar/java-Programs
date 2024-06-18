ffe3bad924aa5731aa322f6a505846800ee0557d
Sukhankar <sukhankarsh@gmail.com>
2024-06-18 13:35:11 +0530

    corrected logic of the code

--- Pallindrom_series.java
+++ Pallindrom_series.java
@@ -6,20 +6,20 @@ public class Pallindrom_series {
         System.out.println("enter the number to check wheather it is the pallindrom series or not:-  ");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        sc.close();
+        // sc.close();
         int num1=num;
         int num2=0;
 
-        while (num1>num2) {
-            num2=num2*10+num%10;
-            num1=num/10;
+        while (num1>0) {
+            num2=num2*10+num1%10;
+            num1=num1/10;
             
         }
 
         System.out.println(num);
         System.out.println(num1);
         System.out.println(num2);
-        if(num1==num2){
+        if(num==num2){
             System.out.println("The given number is pallindrom number ");
         }
         else{
